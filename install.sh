b302daa12b3414fefba89bfe5b68ee83a91a1377
Richard Goforth <dedac@github.com>
2022-10-05 17:49:48 +0000

    Experiment with file copy for vs code settigns syn

---
+++ install.sh
@@ -0,0 +1,6 @@
+ #! bin/bash
+
+ #if we are on codespaces copy the vscode codespaces questions
+if ("$CODESPACES" == "true" ) then
+    cp vscode/settings.json ~/.vscode-remote/data/Machine/settings.json
+fi
