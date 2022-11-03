f719396b872a6cac0ae6a77070f9773e246b16e1
Richard Goforth <dedac@github.com>
2022-11-03 09:33:37 -0400

    Temp remove VSCode settings copy

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
  #! /bin/bash
 
  #if we are on codespaces copy the vscode codespaces questions
-if ("$CODESPACES" == "true" ) then
-    cp vscode/settings.json ~/.vscode-remote/data/Machine/settings.json
-fi
+#if ("$CODESPACES" == "true" ) then
+#    cp vscode/settings.json ~/.vscode-remote/data/Machine/settings.json
+#fi
